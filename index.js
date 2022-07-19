d00bc7c0d98f32a9a00645f193285098f38eee68
João Victor <79557149+iamjvictor@users.noreply.github.com>
2022-07-19 12:47:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ app.use(routes);
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.listen(process.env.PORT || 3000, () =>
+app.listen(port, () =>
   console.log(`Server rodando na porta http://localhost:${port}`)
 );
